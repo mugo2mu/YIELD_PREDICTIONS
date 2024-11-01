c2b67c3c76276ff7d2f4429a9abb668ccfd40cee
mugo2mu <mugo2muiruri@gmail.com>
2024-11-01 00:44:06 -0700

    Initial commit - Crop Yield Prediction project

---
+++ yield.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#import plt.style.use("ggplot")
+
+yield_df=pd.read_csv("C:/machine_learning/yield_df.csv")
+df=yield_df
+df.head(df)
+df.head(df)
+df.corr()
